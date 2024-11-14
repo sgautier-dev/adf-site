f5073ff20f70ac1afa4bca855b874e18b46a67ce
sgautier-dev <sgautier.dev@gmail.com>
2024-11-14 11:58:47 +0400

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    backgroundImage: {
+      'home-bg': "url('/images/home-bg.png')",
+    },
     extend: {
       colors: {
         background: "var(--background)",
