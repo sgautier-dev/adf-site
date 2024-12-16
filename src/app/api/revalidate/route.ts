b178f44ecbaac8f965f254969b0e22d19373bc80
sgautier-dev <sgautier.dev@gmail.com>
2024-12-16 17:07:08 +0400

    added revalidate route

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server"
+import { revalidateTag } from "next/cache"
+
+/*
+Revalidate route: Triggered by a Eventbrite webhook. 
+It retrieves tag and secret from the search params. 
+Revalidate on demand the provided tag.
+*/
+export async function GET(request: NextRequest) {
+	// to use with a webhook: http://localhost:3000/api/revalidate?tag=xxxx&secret=xxxxx
+	const tag = request.nextUrl.searchParams.get("tag")
+	const secret = request.nextUrl.searchParams.get("secret")
+
+	if (secret !== process.env.REVALIDATE_TOKEN || !tag) {
+		return NextResponse.json(
+			{
+				message: "Invalid revalidation params !",
+			},
+			{ status: 401 }
+		)
+	}
+
+	try {
+		revalidateTag(tag)
+		return NextResponse.json({ revalidated: true, now: Date.now() })
+	} catch (err) {
+		console.error(err)
+		return NextResponse.json(
+			{
+				message: "Error revalidating",
+			},
+			{ status: 500 }
+		)
+	}
+}
