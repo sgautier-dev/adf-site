d0d749dd9cb550d0bb3f7d2f0979ddb0b77b2598
sgautier-dev <sgautier.dev@gmail.com>
2025-01-09 09:21:56 +0400

    aria added to language switcher

--- src/app/components/LanguageSwitcher.tsx
+++ src/app/components/LanguageSwitcher.tsx
@@ -9,6 +9,10 @@ export default function LanguageSwitcher() {
 			type="button"
 			onClick={() => switchLanguage(language === "fr" ? "en" : "fr")}
 			className="rounded-full bg-cadetBlue p-1.5 text-white shadow-sm hover:bg-cadetBlue/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cadetBlue text-sm lg:text-base"
+			aria-label={
+				language === "fr" ? "Switch to English" : "Passer en français"
+			}
+			aria-live="polite"
 		>
 			{language === "fr" ? "EN" : "FR"}
 		</button>
