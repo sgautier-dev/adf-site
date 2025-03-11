b2dab94e9cbdbccd49b46aced32233e286eb7dbd
sgautier-dev <sgautier.dev@gmail.com>
2025-03-11 17:04:18 +0400

    added nemo33 event

--- src/app/lib/getCustomEvents.ts
+++ src/app/lib/getCustomEvents.ts
@@ -1,39 +1,20 @@
 import { Translations } from "@/app/lib/translations"
 
+const imageMap: Record<string, string | null> = {
+	nemo33: "/images/visuel-stage.jpg",
+}
 export function getCustomEvents(translations: Translations) {
 	return [
 		{
-			id: "y40",
-			name: translations.customEvents.y40.name,
-			summary: translations.customEvents.y40.summary,
-			startDate: "2025-03-08",
-			endDate: "2025-03-09",
-			imageUrl: "/images/home-bg.png",
-			url: "https://www.y-40.com/en/aqua-dance-flow-julie-gautier-2/",
+			id: "nemo33",
+			name: translations.customEvents.nemo33.name,
+			summary: translations.customEvents.nemo33.summary,
+			startDate: "2025-04-19",
+			endDate: "2025-04-20",
+			imageUrl: imageMap["nemo33"] ?? null,
+			url: "https://www.planeteapnee.fr/aqua-dance-flow-avec-julie-gautier/",
 			isSoldOut: false,
-			city: translations.customEvents.y40.city,
-		},
-		{
-			id: "scubashop",
-			name: translations.customEvents.scubashop.name,
-			summary: translations.customEvents.scubashop.summary,
-			startDate: "2025-09-13",
-			endDate: "2025-09-14",
-			imageUrl: null,
-			url: "https://scubashop.ch/formation/aqua-dance-flow-julie-gautier/",
-			isSoldOut: false,
-			city: translations.customEvents.scubashop.city,
-		},
-		{
-			id: "marinediving",
-			name: translations.customEvents.marinediving.name,
-			summary: translations.customEvents.marinediving.summary,
-			startDate: "2025-09-19",
-			endDate: "2025-09-21",
-			imageUrl: "/images/marine-diving.jpg",
-			url: "https://www.marinedivingcenter.com/evenements/",
-			isSoldOut: false,
-			city: translations.customEvents.marinediving.city,
+			city: translations.customEvents.nemo33.city,
 		},
 	]
 }
