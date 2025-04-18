06533732318413b0395eaac97af7646dd390910f
sgautier-dev <sgautier.dev@gmail.com>
2025-04-18 10:06:18 +0400

    added new adf logo

--- src/app/lib/getCustomEvents.ts
+++ src/app/lib/getCustomEvents.ts
@@ -18,18 +18,5 @@ export function getCustomEvents(translations: Translations): ReturnedEvent[] {
 			banner_sm: translations.customEvents.nemo33.banner_sm,
 			highlighted: true,
 		},
-		{
-			id: "grenoble2025",
-			name: translations.customEvents.grenoble2025.name,
-			summary: translations.customEvents.grenoble2025.summary,
-			startDate: "2025-04-11",
-			endDate: "2025-04-11",
-			imageUrl: null,
-			url: "https://www.univ-grenoble-alpes.fr/actualites/agenda/agenda-sport/histoire-et-genese-d-une-nouvelle-discipline-l-aqua-dance-flow-1565082.kjsp",
-			isSoldOut: false,
-			city: translations.customEvents.grenoble2025.city,
-			banner: translations.customEvents.grenoble2025.banner,
-			banner_sm: translations.customEvents.grenoble2025.banner_sm,
-		},
 	]
 }
