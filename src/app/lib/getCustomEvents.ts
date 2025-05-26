7e57fefd0f7f171b2f7d15bd50ca8b213bff3610
sgautier-dev <sgautier.dev@gmail.com>
2025-05-26 16:12:13 +0400

    new custom event added

--- src/app/lib/getCustomEvents.ts
+++ src/app/lib/getCustomEvents.ts
@@ -4,18 +4,32 @@ import { Translations } from "@/app/lib/translations"
 
 export function getCustomEvents(translations: Translations): ReturnedEvent[] {
 	return [
+		// {
+		// 	id: "nemo33",
+		// 	name: translations.customEvents.nemo33.name,
+		// 	summary: translations.customEvents.nemo33.summary,
+		// 	startDate: "2025-04-19",
+		// 	endDate: "2025-04-20",
+		// 	imageUrl: "/images/visuel-stage.jpg",
+		// 	url: "https://www.planeteapnee.fr/aqua-dance-flow-avec-julie-gautier/",
+		// 	isSoldOut: false,
+		// 	city: translations.customEvents.nemo33.city,
+		// 	banner: translations.customEvents.nemo33.banner,
+		// 	banner_sm: translations.customEvents.nemo33.banner_sm,
+		// 	highlighted: true,
+		// },
 		{
-			id: "nemo33",
-			name: translations.customEvents.nemo33.name,
-			summary: translations.customEvents.nemo33.summary,
-			startDate: "2025-04-19",
-			endDate: "2025-04-20",
-			imageUrl: "/images/visuel-stage.jpg",
-			url: "https://www.planeteapnee.fr/aqua-dance-flow-avec-julie-gautier/",
+			id: "romont",
+			name: translations.customEvents.romont.name,
+			summary: translations.customEvents.romont.summary,
+			startDate: "2025-09-13",
+			endDate: "2025-09-14",
+			imageUrl: null, // mets une image locale ou null si indisponible
+			url: "https://scubashop.ch/formation/aqua-dance-flow-julie-gautier/",
 			isSoldOut: false,
-			city: translations.customEvents.nemo33.city,
-			banner: translations.customEvents.nemo33.banner,
-			banner_sm: translations.customEvents.nemo33.banner_sm,
+			city: translations.customEvents.romont.city,
+			banner: translations.customEvents.romont.banner,
+			banner_sm: translations.customEvents.romont.banner_sm,
 			highlighted: true,
 		},
 	]
