189a70b37c9a26d187727dcdc0baae2d3a841cef
sgautier-dev <sgautier.dev@gmail.com>
2025-09-01 11:27:32 +0200

    added villefranche event

--- src/app/lib/getCustomEvents.ts
+++ src/app/lib/getCustomEvents.ts
@@ -4,20 +4,20 @@ import { Translations } from "@/app/lib/translations"
 
 export function getCustomEvents(translations: Translations): ReturnedEvent[] {
 	return [
-		// {
-		// 	id: "nemo33",
-		// 	name: translations.customEvents.nemo33.name,
-		// 	summary: translations.customEvents.nemo33.summary,
-		// 	startDate: "2025-04-19",
-		// 	endDate: "2025-04-20",
-		// 	imageUrl: "/images/visuel-stage.jpg",
-		// 	url: "https://www.planeteapnee.fr/aqua-dance-flow-avec-julie-gautier/",
-		// 	isSoldOut: false,
-		// 	city: translations.customEvents.nemo33.city,
-		// 	banner: translations.customEvents.nemo33.banner,
-		// 	banner_sm: translations.customEvents.nemo33.banner_sm,
-		// 	highlighted: true,
-		// },
+		{
+			id: "bastia",
+			name: translations.customEvents.bastia.name,
+			summary: translations.customEvents.bastia.summary,
+			startDate: "2025-09-19",
+			endDate: "2025-09-21",
+			imageUrl: "/images/visuel-stage.jpg",
+			url: "https://www.marinedivingcenter.com/evenements/",
+			isSoldOut: false,
+			city: translations.customEvents.bastia.city,
+			banner: translations.customEvents.bastia.banner,
+			banner_sm: translations.customEvents.bastia.banner_sm,
+			highlighted: true,
+		},
 		{
 			id: "romont",
 			name: translations.customEvents.romont.name,
@@ -26,11 +26,25 @@ export function getCustomEvents(translations: Translations): ReturnedEvent[] {
 			endDate: "2025-09-14",
 			imageUrl: null, // mets une image locale ou null si indisponible
 			url: "https://scubashop.ch/formation/aqua-dance-flow-julie-gautier/",
-			isSoldOut: false,
+			isSoldOut: true,
 			city: translations.customEvents.romont.city,
 			banner: translations.customEvents.romont.banner,
 			banner_sm: translations.customEvents.romont.banner_sm,
-			highlighted: true,
+			highlighted: false,
+		},
+		{
+			id: "villefranche",
+			name: translations.customEvents.villefranche.name,
+			summary: translations.customEvents.villefranche.summary,
+			startDate: "2025-08-24",
+			endDate: "2025-08-24",
+			imageUrl: "/images/villefranche-journee-portes-ouvertes.jpeg",
+			url: "https://docs.google.com/forms/d/e/1FAIpQLSclTY9PkNz289NCamXQ-xMOUYkrXZwwGYbNGbVgKAVJoFatrQ/viewform?usp=header",
+			isSoldOut: true,
+			city: translations.customEvents.villefranche.city,
+			banner: translations.customEvents.villefranche.banner,
+			banner_sm: translations.customEvents.villefranche.banner_sm,
+			highlighted: false,
 		},
 	]
 }
